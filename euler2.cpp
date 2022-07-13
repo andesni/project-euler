5670a30522f96a4e2d9e3726f5b7451632876e2d
Sunmisola Shobande <sunmsunmsho>
2022-07-13 09:04:06 +0000

    modified 2

--- euler2.cpp
+++ euler2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	int num1=0, num2=1;
-	int fibonacciSequence;
+	int fibonacciSequence, sum = 0;;
 	
 	cout<<"The sum of even valued terms in the Fibonacci sequence whose values do not exceed four million is ";
 	fibonacciSequence = num1 + num2;
@@ -18,12 +18,24 @@ int main()
 		
 		if(fibonacciSequence%2 == 0)
 		{
-			int sum = 0;
-			fibonacciSequence += sum;
+			sum += fibonacciSequence;
 		}
 	}
 	
-	cout<<fibonacciSequence<<". ";
+//	do
+//	{
+//		num1 = num2;
+//		num2 = fibonacciSequence;
+//		fibonacciSequence = num1 + num2;
+//		
+////	if(fibonacciSequence%2 == 0)
+//		{
+//			int sum = 0;
+//			sum += fibonacciSequence;
+//		}
+//	} while(fibonacciSequence <= 4000000);
+	
+	cout<<sum<<". ";
 
 	
 	return 0;
